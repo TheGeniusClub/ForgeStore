6f4fbb4c274af45bc96a460fb2a7257a67ad664d
deref3046 <fine4trn@163.com>
2026-06-23 20:57:26 +0800

    Revert "raplt vma swap with high-address backup placement remove mprotect fallback"
    This reverts commit 19533c02b33df575ad3608dbddc18e2715477a02.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,10 +51,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures { buildConfig = true }
 
     packaging {
